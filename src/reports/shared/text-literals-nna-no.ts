64bde6927fc9634078d927729a81b357887bbabd
Erik M <89730222+erik-meel@users.noreply.github.com>
2022-03-09 20:42:02 +0100

    NO10 New layout Visit Report

--- src/reports/shared/text-literals-nna-no.ts
+++ src/reports/shared/text-literals-nna-no.ts
@@ -24,6 +24,8 @@
 
   'ORDER_NUMBER': 'Ordrenummer',
 
+  'ORDER_DESCRIPTION': 'Bestillingsbeskrivelse',
+
   'CONTRACT_NUMBER': 'Kontraktsnummer',
 
   'PO_NUMBER': 'PO nummer',
@@ -137,11 +139,19 @@
 
   'SERVICE_ENGINEER_CONFIRMATION': 'Serviceteknikerens bekreftelse',
 
+  'ENTER_YOUR_NAME': 'Skriv inn navnet ditt',
+
   'SIGNED_BY': 'Signert av',
 
   'AT': 'den',
 
+  'LMRA_ENABLED_IN_VISIT_REPORT': 'false',
+
   'BEING_DULY_AUTHORISED': '(Signert på vegne av kunde)',
+
+  'TELEPHONE': 'Tel',
+
+  'FAX': 'Fax',
 
   'COMPANY_GROUP_NAME': 'Atlas Copco Kompressorteknikk AS',
 
@@ -151,7 +161,7 @@
 
   'COMPANY_ADDRESS_2': 'Postadresse: Postboks 336, 1401 Ski',
 
-  'VISITORS_ADDRESS': 'Telefon: 64 86 08 60',
+  'VISITORS_ADDRESS': '',
 
   'VISITOR_ADDRESS_1': '',
 
@@ -187,4 +197,94 @@
   'Score_A':'Stående',
 
   'Score_B':'Liggende',
+
+  //Commissioning Text labels
+  'COMM_TEXT1': 'Commissioning procedure',
+  'COMM_TEXT2': 'The commissioning of a compressor is of special significance. Correct commissioning is the first step in building up the equipment history. The equipment history is essential for proper follow-up. During commissioning the service engineer will record the initial service readings and other operating data of the equipment. All temperatures, pressures, voltage, amps, etc. need to be properly recorded, using the correct service instruments and gauges. The service data in the commissioning report will provide an important source of reference at a later stage. You will refer to them when handling e.g. when discussing any service problem. If the service readings have not recorded during commissioning, then comparison at a later stage may become considerably more difficult.',
+  'COMM_TEXT3': 'Commisioning check',
+  'COMM_TEXT4': 'Check',
+  'COMM_TEXT5': 'Comments',
+  'COMM_Q1_Q': '1. Installation carried out by',
+  'COMM_Q1_A1': 'Atlas Copco',
+  'COMM_Q1_A2': 'Customer',
+  'COMM_Q2_Q': '2. Electrical connection carried out by',
+  'COMM_Q2_A1': 'Atlas Copco',
+  'COMM_Q2_A2': 'Customer',
+  'COMM_Q3_Q': '3. Any damages on the equipment',
+  'COMM_Q3_A1': 'Yes',
+  'COMM_Q3_A2': 'No',
+  'COMM_Q3_A3': 'If yes, where…',
+  'COMM_Q4_Q': '4. Any corrosion found on the equipment',
+  'COMM_Q4_A1': 'Yes',
+  'COMM_Q4_A2': 'No',
+  'COMM_Q4_A3': 'If yes, where…',
+  'COMM_Q5_Q': '5. Environmental condition',
+  'COMM_Q5_A1': 'Clean',
+  'COMM_Q5_A2': 'Dirty',
+  'COMM_Q5_A3': 'Very Dirty',
+  'COMM_Q6_Q': '6. Compressor room properly ventilated',
+  'COMM_Q6_A1': 'Yes',
+  'COMM_Q6_A2': 'No',
+  'COMM_Q7_Q': '7. Any other compressors in the same room',
+  'COMM_Q7_A1': 'Yes',
+  'COMM_Q7_A2': 'No',
+  'COMM_Q7_A3': 'If yes…',
+  'COMM_Q8_Q': '8. Foundation OK',
+  'COMM_Q8_A1': 'Yes',
+  'COMM_Q8_A2': 'No',
+  'COMM_Q9_Q': '9. Inlet and/or outlet ducting used',
+  'COMM_Q9_A1': 'Only inlet',
+  'COMM_Q9_A2': 'Only outlet',
+  'COMM_Q9_A3': 'Both inlet/outlet',
+  'COMM_Q10_Q': '10. Air intake quality OK',
+  'COMM_Q10_A1': 'Yes',
+  'COMM_Q10_A2': 'No',
+  'COMM_Q11_Q': '11. Air header OK',
+  'COMM_Q11_A1': 'Yes',
+  'COMM_Q11_A2': 'No',
+  'COMM_Q12_Q': '12. Air discharge pipe OK',
+  'COMM_Q12_A1': 'Yes',
+  'COMM_Q12_A2': 'No',
+  'COMM_Q13_Q': '13. Air receiver installed',
+  'COMM_Q13_A1': 'Yes',
+  'COMM_Q13_A2': 'No',
+  'COMM_Q13_A3': 'Volume',
+  'COMM_Q13_A4': 'l',
+  'COMM_Q14_Q': '14. Filter(s) installed behind compressor',
+  'COMM_Q14_A1': 'Yes',
+  'COMM_Q14_A2': 'No',
+  'COMM_Q14_A3': 'If yes…',
+  'COMM_Q15_Q': '15. Dryer installed',
+  'COMM_Q15_A1': 'Yes',
+  'COMM_Q15_A2': 'No',
+  'COMM_Q15_A3': 'If yes…',
+  'COMM_Q16_Q': '16. Cooling water quality OK',
+  'COMM_Q16_A1': 'Yes',
+  'COMM_Q16_A2': 'No',
+  'COMM_Q17_Q': '17. Cool water flow l/s',
+  'COMM_Q17_A1': 'Yes',
+  'COMM_Q17_A2': 'No',
+  'COMM_Q17_A3': 'l/s',
+  'COMM_Q18_Q': '18. Drain water goes to',
+  'COMM_Q18_A1': 'Oil/water filter',
+  'COMM_Q18_A2': 'Sewer',
+  'COMM_Q19_Q': '19. Voltage',
+  'COMM_Q20_Q': '20. Frequency',
+  'COMM_Q21_Q': '21. Main motor overload setting',
+  'COMM_Q22_Q': '22. Fan motor(s) overload setting',
+  'COMM_Q23_Q': '23. Load / Unload pressure',
+  'COMM_Q23_A1': 'Bar',
+  'COMM_Q24_Q': '24. Instruction in Elektronikon to customer',
+  'COMM_Q24_A1': 'Yes',
+  'COMM_Q24_A2': 'No',
+  'COMM_Q25_Q': '25. ',
+  'COMM_Q25_A1': 'additional check...',
+  'COMM_Q26_Q': '26. ',
+  'COMM_Q26_A1': 'additional check...',
+  'COMM_Q27_Q': '27. ',
+  'COMM_Q27_A1': 'additional check...',
+  'COMM_Q28_Q': '28. Measurement instrument',
+  'COMM_Q29_Q': '29. Serial Number',
+  'COMM_Q30_Q': '30. Certificate number'
+
 };
