f8e82af688827557037c87be8e1d52d6ae597056
Erik M <89730222+erik-meel@users.noreply.github.com>
2022-09-08 19:52:03 +0200

    Linghein + Bolaite VR+LMRA

---
+++ src/reports/shared/text-literals-cjg-zh.ts
@@ -0,0 +1,124 @@
+export var textLiterals = {
+
+  'VISIT_REPORT': '服务报告',
+
+   'CUSTOMER_DETAILS': '客户信息',
+
+   'CONTACT': '联系人',
+
+   'REPORT_DATE': '报告日期',
+
+   'EQUIPMENT': '机器',
+
+   'RUNNING_HOURS': '运行时间',
+
+   'HOURS': '小时',
+
+   'SERIAL_NUMBER': '序列号',
+
+   'LOAD_HOURS': '载入时间',
+
+   'VISIT_DATE': '访问日期',
+
+   'ORDER_NUMBER': '订单号',
+
+   'ORDER_DESCRIPTION': '工单描述',
+
+   'CONTRACT_NUMBER': '合同号',
+
+   'PO_NUMBER': 'PO号',
+
+   'WORK_COMMENTS': '工作备注',
+
+   'NEXT_VISIT': '下次访问',
+
+   'NEXT_RECOMMENDED_SERVICE_1': '下次服务在 “',
+
+   'NEXT_RECOMMENDED_SERVICE_2': '” 预计时间',
+
+   'FOLLOWING_WORK_IS': '工作建议后,我们的服务销售将与你联系',
+
+   'ESTIMATED_WORK_HOUR': '预计工作时间',
+
+   'TIME_CONFIRMATION': '工时确认',
+
+   'DATE': '日期',
+   'SERVICE_ENGINEER': '服务工程师',
+
+   'ACTIVITY_TYPE': '工作类型',
+   'HOUR_KM': '小时/km',
+
+   'MATERIAL_CONFIRMATION': '物料确认',
+
+   'PART_NUMBER': '物料号',
+
+   'PART_DESCRIPTION': '描述',
+
+   'QUANTITY_UNIT': '数量',
+
+   'WORK_DONE': '工作完成',
+
+   'OPERATIONS': '操作',
+
+   'COMMENTS': '备注',
+
+   'MEASUREMENT_POINTS': '运行数据',
+
+   'COUNTER': '统计',
+
+   'READING_AFTER_SERVICE': '服务后',
+
+   'BEFORE_SERVICE': '服务前',
+
+   'FROM_LAST_VISIT_ON': '上次访问时间',
+
+   'AUTHORISATION_TO_CARRY': 'AUTHORISATION TO CARRY OUT WORK WITH/WITHOUT AN ORDER NO.',
+
+   'NOTES': '备注',
+
+   'CUSTOMER_WILL_AGREE': 'Customer will agree to supply Atlas Copco with a valid order number or payment for non-account holders for actual time spend, travelling and parts used during the breakdown intervention by next working day.',
+
+   'CUSTOMER_CONFIRMATION': '客户确认',
+
+   'SERVICE_ENGINEER_CONFIRMATION': '服务工程师确认',
+
+   'ENTER_YOUR_NAME': '輸入你的名字',
+
+   'SIGNED_BY': '签字',
+
+   'AT': '在',
+
+   'BEING_DULY_AUTHORISED': '(被正式授权代表客户签署)',
+
+  'COMPANY_GROUP_NAME': 'Bolaite (Shanghai) Compressor Co., Ltd.',
+
+  'COMPANY_SLOGAN': 'Better Life Together 美好生活 共同创造',
+
+  'COMPANY_LEGAL_NAME': '博莱特（上海）压缩机有限公司',
+
+  'COMPANY_ADDRESS_1': '上海市宝山区呼兰西路60弄',
+
+  'COMPANY_ADDRESS_2': '卓越时代广场9号楼602-607',
+
+  'VISITOR_ADDRESS_1': '邮编：200443',
+
+  'VISITOR_ADDRESS_2': '销售热线：400-669-0106',
+
+  'FOOTER_CENTER_LINE1': '',
+
+  'COMPANY_FAX': '',
+
+  'COMPANY_URL': 'www.sh-bolaite.com.cn',
+
+  'FOOTER_ADDITIONAL_INFO_1': '服务热线：400-669-8060',
+
+  'FOOTER_ADDITIONAL_INFO_2': '',
+
+  'FOOTER_ADDITIONAL_INFO_3': '',
+
+  'SAFETY_FIRST': 'Safety First',
+
+  'QUESTION': 'Question',
+
+  'CHECKED': 'Checked'
+};
